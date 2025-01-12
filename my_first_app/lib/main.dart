a4eaf99fd4e6691175b2cf092c74f59865bc469d
Doga Mujdee <dogamujde@gmail.com>
2025-01-12 13:50:46 +0000

    commit

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
-main
+import 'package:provider/provider.dart';
+import 'package:my_first_app/login_page.dart';
+import 'package:my_first_app/home_page.dart';
+
+// Theme provider class
+class ThemeProvider with ChangeNotifier {
+  bool _isDarkMode = false;
+
+  bool get isDarkMode => _isDarkMode;
+
+  void toggleTheme() {
+    _isDarkMode = !_isDarkMode;
+    notifyListeners();
+  }
+}
 
 void main() {
   runApp(
@@ -13,7 +27,31 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-main
+    return MaterialApp(
+      title: 'Fridge App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        colorScheme: ColorScheme.light(
+          primary: Color(0xFF10B981), // Emerald color
+          secondary: Color(0xFF10B981),
+          surface: Colors.white,
+          background: Colors.white,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color(0xFF10B981), // Emerald color
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+        useMaterial3: true,
+      ),
+      home: LoginPage(),
+      routes: {
+        '/home': (context) => HomePage(),
       },
     );
   }
